96f9dab0e0ec433474bbca05c1380018fa8e1c29
JulioCezarbc <julio04.cordeiro@gmail.com>
2024-07-21 00:45:46 -0300

    Criado o UserDetails para o Spring consultar os usuarios

---
+++ src/main/java/com/example/login_auth/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.example.login_auth.infra.security;
+
+import com.example.login_auth.domain.user.User;
+import com.example.login_auth.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+
+public class CustomUserDetailsService  implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByEmail(username).orElseThrow(()-> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
