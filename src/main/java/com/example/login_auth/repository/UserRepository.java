05a5989e88385e21d844a2348298d65698825546
JulioCezarbc <julio04.cordeiro@gmail.com>
2024-07-20 23:59:54 -0300

    Conenctando com a classe User

--- src/main/java/com/example/login_auth/repository/UserRepository.java
+++ src/main/java/com/example/login_auth/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.example.login_auth.repository;
 
-public interface UserRepository {
+import com.example.login_auth.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
 }
