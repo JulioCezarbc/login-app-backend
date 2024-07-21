d2f52a0c22657af9ef3a173eecb94f5ffbfe9a5f
JulioCezarbc <julio04.cordeiro@gmail.com>
2024-07-21 00:40:54 -0300

    Adicionado o metodo de buscar user por email

--- src/main/java/com/example/login_auth/repository/UserRepository.java
+++ src/main/java/com/example/login_auth/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.example.login_auth.repository;
 import com.example.login_auth.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
 }
